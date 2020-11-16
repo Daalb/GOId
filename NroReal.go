718e49fa1a11f266b49d9f8c893ad1354414cc3c
Diego Antonio Albor Rojas <daalbor@uninorte.edu.co>
2020-11-16 01:06:04 -0500

    Refex fixed

--- NroReal.go
+++ NroReal.go
@@ -2,15 +2,12 @@ package main
 import (
   "fmt"
   "regexp"
-  "log"
   "os"
   "bufio"
-   
-
 )
 
 func comprobar(a string){
-	r, _ := regexp.Compile("^[-+]?[0-9]+.?[0-9]+([eE][-+]?[0-9]+)?$")
+	r, _ := regexp.Compile("^[-+]?[0-9]+(.[0-9]+([eE][-+]?[0-9]+)?)?$")
 	if r.MatchString(a) == true{
 		fmt.Println(a,"Es un número real")
 	}else{
@@ -18,28 +15,12 @@ func comprobar(a string){
 	} 
 }
 
+
 func main() {
-
-  //Abro el archivo
-  file, err := os.Open("Leer.txt")
-
-  //Manejo de error mientras se abre
-  if err != nil {
-	  log.Fatalf("Error when opening file: %s", err)
+  scanner := bufio.NewScanner(os.Stdin)
+  fmt.Println("Ingrese un número.")
+  for scanner.Scan() {
+    comprobar(scanner.Text())
+    fmt.Println("Ingrese otro número u oprima Ctrl+C para salir.")
   }
-
-  fileScanner := bufio.NewScanner(file)
-
-  //Leer linea por linea  
-  for fileScanner.Scan() {
-	 comprobar(fileScanner.Text())
-
-  }
-
-  //Manejo de error mientras se lee
-  if err := fileScanner.Err(); err != nil {
-	  log.Fatalf("Error while reading file: %s", err)
-  }
-
-  file.Close()
 }
